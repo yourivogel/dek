b4729c719caa587d410778f4faf2abf8b00f7e72
priory <v.packo94@gmail.com>
2019-06-16 12:15:41 +0200

    Include correct database import

--- bek.sql
+++ bek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 13, 2019 at 10:54 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- Host: localhost
+-- Generation Time: Jun 16, 2019 at 10:13 AM
+-- Server version: 8.0.16
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,105 +27,173 @@ USE `bek`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `opdrachten`
+-- Table structure for table `answers`
 --
 
-CREATE TABLE `opdrachten` (
+CREATE TABLE `answers` (
   `id` int(11) NOT NULL,
-  `naam` varchar(255) NOT NULL,
-  `niveau` int(5) NOT NULL
+  `answer` varchar(255) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `correct` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `opdrachten`
+-- Dumping data for table `answers`
 --
 
-INSERT INTO `opdrachten` (`id`, `naam`, `niveau`) VALUES
-(1, 'modals', 2);
+INSERT INTO `answers` (`id`, `answer`, `question_id`, `correct`) VALUES
+(1, 'a', 1, 1),
+(2, 'a', 2, 0),
+(3, 'b', 2, 1),
+(4, 'c', 4, 1),
+(5, 'd', 3, 1),
+(6, 'e', 3, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vragen`
+-- Table structure for table `exercises`
 --
 
-CREATE TABLE `vragen` (
+CREATE TABLE `exercises` (
   `id` int(11) NOT NULL,
-  `vraag` varchar(255) NOT NULL,
-  `antwoord` varchar(255) NOT NULL,
-  `opdracht_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `level_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
--- Dumping data for table `vragen`
+-- Dumping data for table `exercises`
 --
 
-INSERT INTO `vragen` (`id`, `vraag`, `antwoord`, `opdracht_id`) VALUES
-(1, 'asd?', 'a', 1);
+INSERT INTO `exercises` (`id`, `name`, `level_id`) VALUES
+(1, 'Modals 1', 1),
+(2, 'Modals 2', 2),
+(3, 'Future 1', 1),
+(4, 'Future 2', 1),
+(5, 'Comparative 1', 2),
+(6, 'Comparative 2', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `woorden`
+-- Table structure for table `levels`
 --
 
-CREATE TABLE `woorden` (
+CREATE TABLE `levels` (
   `id` int(11) NOT NULL,
-  `woord_NL` varchar(255) NOT NULL,
-  `woord_EN` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `levels`
+--
+
+INSERT INTO `levels` (`id`, `name`) VALUES
+(1, 'Level 1 (beginner)'),
+(2, 'Level 2 (intermediate)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `question` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exercise_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `question`, `exercise_id`) VALUES
+(1, 'what is a?', 1),
+(2, 'What is b?', 1),
+(3, 'what is d?', 2),
+(4, 'what is c?', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `opdrachten`
+-- Indexes for table `answers`
 --
-ALTER TABLE `opdrachten`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `vragen`
---
-ALTER TABLE `vragen`
+ALTER TABLE `answers`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `vraag_opdracht` (`opdracht_id`);
+  ADD KEY `question_id` (`question_id`);
 
 --
--- Indexes for table `woorden`
+-- Indexes for table `exercises`
 --
-ALTER TABLE `woorden`
+ALTER TABLE `exercises`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `exercises_level_id_foreign` (`level_id`);
+
+--
+-- Indexes for table `levels`
+--
+ALTER TABLE `levels`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vraag_opdracht` (`exercise_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `opdrachten`
+-- AUTO_INCREMENT for table `answers`
 --
-ALTER TABLE `opdrachten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
--- AUTO_INCREMENT for table `vragen`
+-- AUTO_INCREMENT for table `exercises`
 --
-ALTER TABLE `vragen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `exercises`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
--- AUTO_INCREMENT for table `woorden`
+-- AUTO_INCREMENT for table `levels`
 --
-ALTER TABLE `woorden`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `levels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `vragen`
+-- Constraints for table `answers`
 --
-ALTER TABLE `vragen`
-  ADD CONSTRAINT `vraag_opdracht` FOREIGN KEY (`opdracht_id`) REFERENCES `opdrachten` (`id`);
+ALTER TABLE `answers`
+  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
+
+--
+-- Constraints for table `exercises`
+--
+ALTER TABLE `exercises`
+  ADD CONSTRAINT `exercises_level_id_foreign` FOREIGN KEY (`level_id`) REFERENCES `levels` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_exercise_id_foreign` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
